1bd0376219d48d7567cd4fe8242c9839ea0ea9ea
jwala-certified <120051211+jwala-certified@users.noreply.github.com>
2022-12-07 12:28:57 -0600

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ActivityBoredAPI._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <p class="lead">Please select your prefrence :
+            <asp:DropDownList ID="ddlPreference" runat="server" ClientIDMode="AutoID" DataMember="Education"></asp:DropDownList>
+        </p>
+        <p>
+            <asp:Button ID="btnGetActivity" runat="server" Text="Get Activity" CssClass="btn btn-primary btn-lg" OnClick="btnGetActivity_Click" />
+        </p>
+    </div>
+    
+    <div id ="dvContent" runat="server">
+        <button type="button" id="btn" runat="server" class="collapsible"></button>
+        <div class="content">
+            <label id="lblType" runat="server"></label><br />
+            <label id="lblNP" runat="server"></label><br />
+            <label id="lblRelPrice" runat="server"></label><br />
+            <label id="lblLink" runat="server"></label><br />
+            <label id="lblAccRating" runat="server"></label><br />
+        </div>
+    </div>
+
+    <script>
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
+        }
+    </script>
+</asp:Content>
+
+
---
+++ Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace ActivityBoredAPI
+{
+    public partial class _Default : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                ddlPreference.Items.Add(new ListItem("Education", "education"));
+                ddlPreference.Items.Add(new ListItem("Recreational", "recreational"));
+                ddlPreference.Items.Add(new ListItem("Social", "social"));
+                ddlPreference.Items.Add(new ListItem("DIY", "diy"));
+                ddlPreference.Items.Add(new ListItem("Charity", "charity"));
+                ddlPreference.Items.Add(new ListItem("Cooking", "cooking"));
+                ddlPreference.Items.Add(new ListItem("Relaxation", "relaxation"));
+                ddlPreference.Items.Add(new ListItem("Music", "music"));
+                ddlPreference.Items.Add(new ListItem("Busywork", "busywork"));
+
+
+            }
+        }
+
+        protected void btnGetActivity_Click(object sender, EventArgs e)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://www.boredapi.com/api/");
+            
+            var consumeapi = hc.GetAsync("activity?type=" + ddlPreference.SelectedValue.ToString());
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if(readdata.IsSuccessStatusCode)
+            {
+                var displayresults = readdata.Content.ReadAsStringAsync();
+                Activity act = JsonConvert.DeserializeObject<Activity>(displayresults.Result);
+
+                btn.InnerText = act.activity;
+                lblType.InnerText = "Type : " + act.type;
+                lblNP.InnerText = "Number of Participants : " + act.participants;
+                lblRelPrice.InnerText = "Relative Price : " + act.price;
+                if (string.IsNullOrEmpty(act.link) == false)
+                    lblLink.InnerText = "Link : " + act.link;
+                lblAccRating.InnerText = "Accessibility rating : " + act.accessibility;
+            }
+
+        }
+
+    }
+
+    public class Activity
+    {
+        public string activity { get; set; }
+        public string type { get; set; }
+        public string participants { get; set; }
+        public string price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public string accessibility { get; set; }
+    }
+}
